2e1d6ac7efb5a168e13a3470f21986ba0bc13671
z3451444 <z3451444@cse.unsw.edu.au>
2019-11-17 15:47:14 +1100

    Added feature to emit final inputs

--- src/AssigTwoz3451444.java
+++ src/AssigTwoz3451444.java
@@ -1,14 +1,22 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
+import scala.Tuple3;
 
 public class AssigTwoz3451444 {
 
@@ -44,16 +52,103 @@ public class AssigTwoz3451444 {
 		//DEBUG Print Raw inputs
 		System.out.println("Start node: " + init_start_node);
 		
-		// Transformation: input RDD to a list of string
+		// Transformation 1: input RDD to a list of string
 		JavaPairRDD<String,Tuple2<String,Integer>> inputPairs = inputRDD.mapToPair(line -> {
 			String[] items = line.split(",");
 			Tuple2<String, Integer> destAndDistPair = new Tuple2<String, Integer>(items[1], Integer.parseInt(items[2]));
 			return new Tuple2<String,Tuple2<String,Integer>>(items[0],destAndDistPair);
 		});
 		
+		// Get total number of nodes and their names
+		File tempFile = new File("TEMP/temp.txt");
+		FileUtils.touch(tempFile);
+		inputPairs.foreach(pair -> {
+			String curNode = pair._1;
+			String destNode = pair._2._1;
+			List<String> nodeNames = Arrays.asList(FileUtils.readFileToString(tempFile).split(","));
+			if (!nodeNames.contains(curNode)) {
+				if (tempFile.length()!=0) 
+					FileUtils.writeStringToFile(tempFile,",",true);
+				FileUtils.writeStringToFile(tempFile,curNode,true);
+			} 
+				
+			if (curNode != destNode && !nodeNames.contains(destNode)) {
+				if (tempFile.length()!=0) 
+					FileUtils.writeStringToFile(tempFile,",",true);
+				FileUtils.writeStringToFile(tempFile,destNode,true);
+			}
+		});
+		int numberOfNodes = Arrays.asList(FileUtils.readFileToString(tempFile).split(",")).size();
+		
 		//DEBUG Print input pair
+		System.out.println("Total number of nodes: " + Integer.toString(numberOfNodes));
 		System.out.println("Pairs after first transformation.");
 		inputPairs.collect().forEach(System.out::println);
+		
+		// Action: Group input by current nodes
+		JavaPairRDD<String,Iterable<Tuple2<String,Integer>>> inputWithAdjList = inputPairs.groupByKey();
+		
+		//DEBUG Print group outputs
+		System.out.println("Group input pairs by key.");
+		inputWithAdjList.collect().forEach(System.out::println);
+		
+		// Transformation: Add path and distance to start node
+		JavaPairRDD<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>> inputWithPathAndAdjList = 
+				inputWithAdjList.mapToPair(item -> {
+					String curNode = item._1;
+					int distFromStartToCurNode = Integer.MAX_VALUE;
+					if (init_start_node.equals(curNode))
+						distFromStartToCurNode = 0;
+					Iterable<String> path = Arrays.asList(curNode);
+					Iterable<Tuple2<String,Integer>> adjacentList = item._2;
+					return new Tuple2<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>>(
+							curNode,new Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>(
+									distFromStartToCurNode,path,adjacentList));
+				});
+		
+		//DEBUG Print final inputs
+		System.out.println("Final inputs with path and adj list");
+		inputWithPathAndAdjList.collect().forEach(System.out::println);
+		
+		// Transformation: Emit
+		JavaPairRDD<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>> emiitedPairs =
+				inputWithPathAndAdjList.flatMapToPair(item -> {
+					// Add current item to the emitted pairs list
+					List<Tuple2<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>>> emittedPairs =
+						new ArrayList<Tuple2<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>>>();
+					emittedPairs.add(item);
+					// Iterate through adjacent nodes
+					String curNode = item._1;
+					int curDist = item._2._1();
+					Iterable<String> curPath = item._2._2();
+					Iterable<Tuple2<String,Integer>> adjList = item._2._3();
+					for(Tuple2<String,Integer> adjItem:adjList) {
+						String adjNode = adjItem._1;
+						int adjDist = adjItem._2;
+						int updatedDist = Integer.MAX_VALUE;
+						List<String> updatedPath = new ArrayList<String>();
+						// Update distance
+						if (curDist != Integer.MAX_VALUE) {
+							updatedDist = curDist + adjDist;
+						}
+						// Update path
+						Iterator<String> curPathIterator = curPath.iterator();
+						while(curPathIterator.hasNext())
+							updatedPath.add(curPathIterator.next());
+						updatedPath.add(adjNode);
+						// Add emitted item to the mapping output
+						Iterable<Tuple2<String,Integer>> emptyAdjList = 
+								new ArrayList<Tuple2<String,Integer>>();
+						Tuple2<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>> newlyEmiitedPair =
+								new Tuple2<String,Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>>(adjNode,
+										new Tuple3<Integer,Iterable<String>,Iterable<Tuple2<String,Integer>>>(updatedDist,updatedPath,emptyAdjList));
+						emittedPairs.add(newlyEmiitedPair);
+					}
+					return emittedPairs.iterator();
+				});
+		//DEBUG Print after emit
+		System.out.println("After emittion:");
+		emiitedPairs.collect().forEach(System.out::println);
 	}
 
 }
